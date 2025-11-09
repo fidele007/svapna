5cbf1ee1e84c5ca45b66fcdb598b00006de94b8a
Force Fidele KIEN <forcefidele@gmail.com>
2025-11-09 22:22:20 +0100

    feat: Use NavigationRail for big screen

--- lib/app.dart
+++ lib/app.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:svapna/i18n/app_localizations.dart';
 import 'package:svapna/screens/about_screen.dart';
 import 'package:svapna/screens/bookmark_screen.dart';
-import 'package:svapna/screens/home_screen.dart';
 import 'package:svapna/screens/history_screen.dart';
+import 'package:svapna/screens/home_screen.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -34,43 +34,78 @@ class _AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
+    final isWide = MediaQuery.of(context).size.width >= 850;
+
     return Scaffold(
-      body: PageView(
-        controller: _pageController,
-        physics: const NeverScrollableScrollPhysics(),
-        children: _pages,
-      ),
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            _currentPageIndex = index;
-            _pageController.jumpToPage(index);
-          });
-        },
-        selectedIndex: _currentPageIndex,
-        destinations: <NavigationDestination>[
-          NavigationDestination(
-            icon: Icon(Icons.home_outlined),
-            selectedIcon: Icon(Icons.home),
-            label: AppLocalizations.of(context)!.home,
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.history_outlined),
-            selectedIcon: Icon(Icons.history),
-            label: AppLocalizations.of(context)!.history,
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.bookmark_outline),
-            selectedIcon: Icon(Icons.bookmark),
-            label: AppLocalizations.of(context)!.bookmarks,
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.info_outline),
-            selectedIcon: Icon(Icons.info),
-            label: AppLocalizations.of(context)!.about,
+      body: Row(
+        children: [
+          if (isWide)
+            NavigationRail(
+              useIndicator: true,
+              labelType: NavigationRailLabelType.all,
+              destinations: <NavigationRailDestination>[
+                NavigationRailDestination(
+                  icon: Icon(Icons.home_outlined),
+                  selectedIcon: Icon(Icons.home),
+                  label: Text(AppLocalizations.of(context)!.home),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.history_outlined),
+                  selectedIcon: Icon(Icons.history),
+                  label: Text(AppLocalizations.of(context)!.history),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.bookmark_outline),
+                  selectedIcon: Icon(Icons.bookmark),
+                  label: Text(AppLocalizations.of(context)!.bookmarks),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.info_outline),
+                  selectedIcon: Icon(Icons.info),
+                  label: Text(AppLocalizations.of(context)!.about),
+                ),
+              ],
+              selectedIndex: _currentPageIndex,
+              onDestinationSelected: onDestinationSelected,
+            ),
+          if (isWide) const VerticalDivider(thickness: 1, width: 1),
+          Expanded(
+            child: PageView(
+              controller: _pageController,
+              physics: const NeverScrollableScrollPhysics(),
+              children: _pages,
+            ),
           ),
         ],
       ),
+      bottomNavigationBar: isWide
+          ? null
+          : NavigationBar(
+              onDestinationSelected: onDestinationSelected,
+              selectedIndex: _currentPageIndex,
+              destinations: <NavigationDestination>[
+                NavigationDestination(
+                  icon: Icon(Icons.home_outlined),
+                  selectedIcon: Icon(Icons.home),
+                  label: AppLocalizations.of(context)!.home,
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.history_outlined),
+                  selectedIcon: Icon(Icons.history),
+                  label: AppLocalizations.of(context)!.history,
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.bookmark_outline),
+                  selectedIcon: Icon(Icons.bookmark),
+                  label: AppLocalizations.of(context)!.bookmarks,
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.info_outline),
+                  selectedIcon: Icon(Icons.info),
+                  label: AppLocalizations.of(context)!.about,
+                ),
+              ],
+            ),
     );
   }
 
@@ -79,5 +114,12 @@ class _AppState extends State<App> {
     _pageController.dispose();
 
     super.dispose();
+  }
+
+  void onDestinationSelected(int index) {
+    setState(() {
+      _currentPageIndex = index;
+      _pageController.jumpToPage(index);
+    });
   }
 }
